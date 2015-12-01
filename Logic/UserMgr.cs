faa638b62be85e0cdc7752c8cc946756b4e503bb
wjkonger@gmail.com <wjkonger@gmail.com>
2015-11-30 19:02:25 -0500

    Encrypt password

--- Logic/UserMgr.cs
+++ Logic/UserMgr.cs
@@ -1,28 +1,34 @@
 ﻿using Kong.OnlineStoreAPI.DAL;
 using Kong.OnlineStoreAPI.Model;
 using System;
+using System.Configuration;
 
 namespace Kong.OnlineStoreAPI.Logic
 {
     public class UserMgr
     {
+        private string passPhrase = ConfigurationManager.AppSettings["passPhrase"].ToString();
+
         protected UserDacMgr dacMgr = new UserDacMgr();
 
         public bool Login(User info)
         {
             info.Status = "A";
+            info.Password = StringCipher.Encrypt(info.Password, passPhrase);
 
             return dacMgr.Login(info);
         }
         
         public bool Add(User info)
         {
+            info.Password = StringCipher.Encrypt(info.Password, passPhrase);
             return dacMgr.Insert(info);
         }
 
         public bool Modify(User info)
         {
             info.UpdatedDate = DateTime.Now;
+            info.Password = StringCipher.Encrypt(info.Password, passPhrase);
 
             return dacMgr.Update(info);
         }
@@ -32,7 +38,7 @@ namespace Kong.OnlineStoreAPI.Logic
             bool success = false;
 
             info.UpdatedDate = DateTime.Now;
-            info.Password = LogicHelper.RandomString(6);
+            info.Password = StringCipher.Encrypt(LogicHelper.RandomString(6), passPhrase);
             info.Status = NUserStatus.ChangePassword.GetStrValue(); ;
 
             if (dacMgr.UpdateStatus(info))
